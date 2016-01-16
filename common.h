b6addb00fe002148a8a4b0afc864a39788a08b45
spacecamper <spacecamper@yandex.com>
2016-01-16 20:08:16 +0100

    Create common.h

---
+++ common.h
@@ -0,0 +1,13 @@
+#define GAME_INITIALIZED -1
+#define GAME_PLAYING 0
+#define GAME_LOST 1
+#define GAME_WON 2
+
+#define MAX_WIDTH 100
+#define MAX_HEIGHT 100
+
+
+#define BORDER_WIDTH 10
+#define DISPLAY_BORDER_WIDTH 4
+#define FIELD_X 10
+#define FIELD_Y 48
