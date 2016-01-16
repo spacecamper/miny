937724aa7617c4db293241af6d902ed6ecc66d5d
spacecamper <spacecamper@yandex.com>
2016-01-16 20:09:25 +0100

    Create Replay.h

---
+++ Replay.h
@@ -0,0 +1,66 @@
+
+using namespace std;
+
+class ReplayPoint {
+public:
+    
+    int x, y;
+    int button;
+    long timeSinceStart;
+
+    ReplayPoint();
+
+    ReplayPoint(int, int, int, long);
+
+    void dump();
+
+};
+
+
+void mouseClick(int,int,int,int);
+
+
+
+class Replay {
+public:
+
+    std::list<ReplayPoint> data;
+    std::list<ReplayPoint>::iterator nextPlayed;
+
+    bool recording;
+    bool endOfReplay;
+
+
+    int cursorX,cursorY;
+
+
+    Replay();
+
+    bool isRecording();
+
+
+    void startRecording();
+
+    void pauseRecording();
+
+    void resumeRecording();
+
+    void stopRecording();
+
+
+
+    void deleteData();
+
+    void recordEvent(int, int, int);
+
+
+    void writeToFile(ofstream*);
+
+
+    void readFromFile(ifstream*);
+
+    void dump();
+    
+    unsigned int playStep();
+
+};
