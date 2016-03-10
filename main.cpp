5a39215489efb1afdfbe15ddedbab946438b4aaf
spacecamper <spacecamper@users.noreply.github.com>
2016-03-10 13:27:37 +0100

    Update main.cpp
    Incremented version number to reflect the previous bugfixes.

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@
 #include "scores.h"
 
 
-#define VERSION "0.5.0"
+#define VERSION "0.5.1"
 
 // TODO elapsed time isn't being redrawn while playing replay when there's a long pause between two 
 //   events
